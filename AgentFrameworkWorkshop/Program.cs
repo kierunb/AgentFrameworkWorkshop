fc7dc7cb1f4e7d825c1e05ce529a8639f5ae0e16
Bartek <kierunb@gmail.com>
2026-04-24 12:23:00 +0200

    app

--- AgentFrameworkWorkshop/Program.cs
+++ AgentFrameworkWorkshop/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.Agents.AI;
+using Microsoft.Agents.AI.DevUI;
+using Microsoft.Agents.AI.Hosting;
+using Microsoft.Extensions.AI;
 using Microsoft.ML.OnnxRuntimeGenAI;
 using OpenAI;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +17,14 @@ Console.WriteLine("Model loaded...");
 
 builder.Services.AddChatClient(chatClient);
 
+builder.AddAIAgent(
+    name: "agent2",
+    instructions: "A helpful assistant that can answer questions and provide information.");
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddOpenAIResponses();
+builder.Services.AddOpenAIConversations();
 
 var app = builder.Build();
 
@@ -20,9 +32,34 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("/scalar");
+    app.MapDevUI(); // /devui
 }
 
+app.MapOpenAIResponses();
+app.MapOpenAIConversations();
+
 app.MapGet("/", () => "Hello World!");
+
+
+app.MapGet("/chat/{prompt}", async (string prompt, IChatClient chatClient) =>
+{
+    var agent = chatClient.AsAIAgent(
+        name: "agent1", 
+        instructions: "A helpful assistant that can answer questions and provide information.");
+    
+    var response = await agent.RunAsync(prompt);
+
+    return Results.Ok(response);
+});
+
+
+app.MapGet("/agent/{prompt}", async (string prompt, [FromKeyedServices("agent2")]AIAgent agent) =>
+{
+    var response = await agent.RunAsync(prompt);
+    return Results.Ok(response);
+});
+
 
 app.UseHttpsRedirection();
 
